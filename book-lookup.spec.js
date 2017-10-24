5fbe4afbee2b8639cb13098a1b0ac34d4ec6a6fe
yoduta <knotinw2540@gmail.com>
2017-10-24 15:10:47 +0700

    add function bbooklookup

--- book-lookup.spec.js
+++ book-lookup.spec.js
@@ -1,6 +1,21 @@
+function BookLookup(){
+    
+    this.search =(isbn) =>{
+        return this.AmazonService(isbn)
+    }
+    
+
+
+
+}
+
+
+
 test('BookLookup',() =>{
+    const AmazonServices = jest.fn('1234567890123')
+    .mockReturnValue({name: 'basic java',cover:'javabasic.com',isbn:'1234567890123'})
     var app = new BookLookup(AmazonServices)
     var isbn = '1234567890123'
     var value = app.search(isbn)
-    
+
 })
