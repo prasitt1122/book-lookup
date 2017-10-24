30af057ba920988728716808a51fd8d4b7609ea2
yoduta <knotinw2540@gmail.com>
2017-10-24 15:34:49 +0700

    check value equal

--- book-lookup.spec.js
+++ book-lookup.spec.js
@@ -2,7 +2,8 @@ function BookLookup(AmazonServices){
     this.AmazonServices = AmazonServices
     
     this.search =(isbn) =>{
-        return this.AmazonService(isbn)
+        let Search = this.AmazonServices(isbn)
+        return Search
     }
     
 
@@ -14,9 +15,12 @@ function BookLookup(AmazonServices){
 
 test('BookLookup',() =>{
     const AmazonServices = jest.fn('1234567890123')
-    .mockReturnValue({name: 'basic java',cover:'javabasic.com',isbn:'1234567890123'})
-    var app = new BookLookup(AmazonServices)
-    var isbn = '1234567890123'
-    var value = app.search(isbn)
+    .mockReturnValue({name:'basic java',cover:'javabasic.com',isbn:'1234567890123'})
+    let app = new BookLookup(AmazonServices)
+    let isbn = '1234567890123'
+    let value = app.search(isbn)
+
+
+    expect(value).toEqual({name:'basic java',cover:'javabasic.com',isbn:'1234567890123'})
 
 })
